4f4189133b13b44438d3dd0554d8029c801129bf
Bernardo Donadio <bcdonadio@bcdonadio.com>
2017-01-02 08:00:36 +0000

    Suppress annoying pip messages

--- files/powerline-bash.sh
+++ files/powerline-bash.sh
@@ -4,7 +4,8 @@
 if [ -n "${BASH_VERSION-}" ]; then
     systemctl status powerline.service &>/dev/null
     if [ $? -eq 0 ]; then
-        POWERLINE_PATH="$( pip show powerline-status | grep -Po '^Location: \K(/.*)$' )"
+        POWERLINE_PATH="$( pip show powerline-status 2>/dev/null |\
+            grep -Po '^Location: \K(/.*)$' )"
         POWERLINE_BASH_SELECT=1
         POWERLINE_BASH_CONTINUATION=1
         POWERLINE_CONFIG_PATHS="/etc/powerline,$HOME/.config/powerline"
