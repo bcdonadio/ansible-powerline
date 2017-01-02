b4186974fbf749fb97b6ad91aa0cb59b351897dc
Bernardo Donadio <bcdonadio@bcdonadio.com>
2017-01-02 10:01:08 +0000

    Don't use a systemd service

--- files/powerline-bash.sh
+++ files/powerline-bash.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 # This script will source powerline configurations for Bash
 
-if [ -n "${BASH_VERSION-}" ]; then
-    systemctl status powerline.service &>/dev/null
-    if [ $? -eq 0 ]; then
-        POWERLINE_PATH="$( pip show powerline-status 2>/dev/null |\
-            grep -Po '^Location: \K(/.*)$' )"
-        POWERLINE_BASH_SELECT=1
-        POWERLINE_BASH_CONTINUATION=1
-        POWERLINE_CONFIG_PATHS="$POWERLINE_PATH/powerline/config_files:/etc/powerline:$HOME/.config/powerline"
-        source "$POWERLINE_PATH/powerline/bindings/bash/powerline.sh"
-    fi
+if [ -n "${BASH_VERSION-}" ] && [ -f `which powerline-daemon` ]; then
+    powerline-daemon -q
+    POWERLINE_PATH="$( pip show powerline-status 2>/dev/null |\
+        grep -Po '^Location: \K(/.*)$' )"
+    POWERLINE_BASH_SELECT=1
+    POWERLINE_BASH_CONTINUATION=1
+    POWERLINE_CONFIG_PATHS="$POWERLINE_PATH/powerline/config_files:/etc/powerline:$HOME/.config/powerline"
+    source "$POWERLINE_PATH/powerline/bindings/bash/powerline.sh"
 fi
 
